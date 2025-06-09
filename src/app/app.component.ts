201038fccd393c5ae2e89f0cf5e3574606ff5cc3
Gennaro Di Fiandra <gennarodifiandra@gmail.com>
2025-06-09 09:51:25 +0200

    (feat) Complete App component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ResearcherComponent } from './researcher/researcher.component';
+import { ResultsComponent } from './results/results.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [HeaderComponent, FooterComponent, ResearcherComponent, ResultsComponent],
   templateUrl: './app.component.html',
+  styles: `:host {@apply min-h-screen flex flex-col [&>*:last-child]:mt-auto}`,
 })
 export class AppComponent {}
