6bbc18d6aff59ba01fb5654fdbdc94ef5aa1bd06
Gennaro Di Fiandra <gennarodifiandra@gmail.com>
2025-06-09 09:49:47 +0200

    (feat) Add Ip2Location service

---
+++ src/app/shared/services/ip-2-location.service.ts
@@ -0,0 +1,42 @@
+import { inject, Injectable, signal } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError, tap, throwError } from 'rxjs';
+import { environment } from '../../../environments/environment.development';
+import { Ip2WhoIsError, Ip2WhoIsResponse } from '../../researcher/researcher.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Ip2LocationService {
+  private http = inject(HttpClient);
+  domainDetails = signal<Ip2WhoIsResponse | null>(null);
+  whoIsError = signal<string | null>(null);
+
+  getDomainDetails(domain: string) {
+    return this.http
+      .get<Ip2WhoIsResponse>(environment.ip2locationConfig.endpoint, {
+        params: { key: environment.ip2locationConfig.apiKey, domain: domain },
+      })
+      .pipe(
+        tap({
+          next: (data: Ip2WhoIsResponse) => {
+            this.domainDetails.set(data);
+            this.whoIsError.set(null);
+          },
+        }),
+        catchError((error: HttpErrorResponse) => {
+          if (error.error instanceof ErrorEvent) {
+            this.whoIsError.set(error.message);
+          } else if ((error.error as Ip2WhoIsError)?.error.error_message !== undefined) {
+            this.whoIsError.set((error.error as Ip2WhoIsError).error.error_message);
+          } else {
+            this.whoIsError.set(error.message);
+          }
+
+          this.domainDetails.set(null);
+
+          return throwError(() => error);
+        })
+      );
+  }
+}
