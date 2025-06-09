57cf4b517f9728ec192af115ed9caccfb0be6628
Gennaro Di Fiandra <gennarodifiandra@gmail.com>
2025-06-09 10:16:01 +0200

    (chore) Setup Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,28 @@
-export const environment = {};
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+
+const firebaseConfig = {
+  apiKey: 'FIREBASE_API_KEY_PLACEHOLDER',
+  authDomain: 'angular-apps-87b70.firebaseapp.com',
+  projectId: 'angular-apps-87b70',
+  storageBucket: 'angular-apps-87b70.firebasestorage.app',
+  messagingSenderId: '729379065638',
+  appId: 'FIREBASE_APP_ID_PLACEHOLDER',
+  measurementId: 'FIREBASE_MEASUREMENT_ID_PLACEHOLDER',
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const ip2locationConfig = {
+  endpoint: '/ip2whois/v2',
+  apiKey: 'IP2L_API_KEY_PLACEHOLDER',
+};
+
+export const environment = {
+  production: true,
+  firebaseConfig: firebaseConfig,
+  firebaseApp: app,
+  firebaseAnalytics: analytics,
+  ip2locationConfig: ip2locationConfig,
+};
